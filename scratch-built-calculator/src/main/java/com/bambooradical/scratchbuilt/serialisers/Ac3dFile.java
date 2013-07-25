d9a9a315d5e6e96d5306358675ae44795d5449d2
PeterWithers <peter-gthb@bambooradical.com>
2013-07-25 21:35:54 +0200

    Updated the ac3d file for multi segment wing types.

--- scratch-built-calculator/src/main/java/com/bambooradical/scratchbuilt/serialisers/Ac3dFile.java
+++ scratch-built-calculator/src/main/java/com/bambooradical/scratchbuilt/serialisers/Ac3dFile.java
@@ -165,7 +165,7 @@ public class Ac3dFile {
         double lastLength = 0;
         for (double[] lengthAngle : type.getLengths()) {
             returnString += nextKids + "OBJECT poly\n"
-                    + "loc " + 0 + " " + -lastLength + " " + 0 + "\n"
+                    + "loc " + 0 + " " + 0 + " " + lastLength + "\n"
                     + getRotationMatrix(lengthAngle[1], 0, 0)
                     + "kids 2\n"
                     + getWing(0, lengthAngle[0] * chord, start, length, thickness, 0, 0, 2);
