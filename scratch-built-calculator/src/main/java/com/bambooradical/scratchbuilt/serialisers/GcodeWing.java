c7153d9853044e5ca561a5f7cd6453bbc042b334
PeterWithers <peter-gthb@bambooradical.com>
2013-10-07 19:47:26 +0200

    Added a class and REST service to return GCODE for a 3D printer based on a given aerofoil data set, chord and span.

--- scratch-built-calculator/src/main/java/com/bambooradical/scratchbuilt/serialisers/GcodeWing.java
+++ scratch-built-calculator/src/main/java/com/bambooradical/scratchbuilt/serialisers/GcodeWing.java
@@ -23,6 +23,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -34,31 +36,58 @@ public class GcodeWing {
 
     // all units are in millimeters
     final private ModelData modelData;
+    final private String aerofoilDataFile = "/AerofoilData/n6409.dat";
     final private double layerHeight = 0.2;
     final private double targetHeight = 40.0;
     final private double targetChord = 80.0;
     private double currentX = 0;
     private double currentY = 0;
     private double currentZ = 0;
+    private double currentA = 0;
     private double currentPercent = 0;
+    private int primeSpeed = 1500;
+    private int travelSpeed = 3000;
+    private int extrudeSpeed = 1800;
 
     public GcodeWing(ModelData modelData) {
         this.modelData = modelData;
     }
 
     public void getGcode(OutputStream output) throws IOException {
-        final InputStream aerofoilData = GcodeWing.this.getClass().getResourceAsStream("/AerofoilData/n6409.dat");
-        Scanner scanner = new Scanner(aerofoilData);
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(output));
-        bufferedWriter.write("start of file");
-        while (scanner.hasNext()) {
-            bufferedWriter.write(scanner.nextLine());
-        }
+        writeInformativeHeader(bufferedWriter);
+        selectExtruder(bufferedWriter);
+        List<double[]> aerofoilData = getAerofoilData();
         while (currentZ < targetHeight) {
             setNextLayer(bufferedWriter);
+            writeLayer(bufferedWriter, aerofoilData);
             writePercentDone(bufferedWriter, targetHeight, currentZ);
         }
         bufferedWriter.close();
+    }
+
+    private List<double[]> getAerofoilData() {
+        List pointsList = new ArrayList<double[]>();
+        final InputStream aerofoilData = GcodeWing.this.getClass().getResourceAsStream(aerofoilDataFile);
+        Scanner scanner = new Scanner(aerofoilData);
+        while (scanner.hasNext()) {
+            while (!scanner.hasNextDouble()) {
+                scanner.next();
+            }
+            pointsList.add(new double[]{scanner.nextDouble(), scanner.nextDouble()});
+        }
+        return pointsList;
+    }
+
+    private void writeInformativeHeader(BufferedWriter bufferedWriter) throws IOException {
+        bufferedWriter.write("; Aerofoil gcode produced by scratchbuiltcalculator: https://github.com/PeterWithers/ScratchBuiltCalculator\r\n");
+        bufferedWriter.write("; Using aerofoil data file: " + aerofoilDataFile + "\r\n");
+        bufferedWriter.write("; chord length: " + targetChord + "\r\n");
+        bufferedWriter.write("; span length: " + targetHeight + "\r\n");
+    }
+
+    private void selectExtruder(BufferedWriter bufferedWriter) throws IOException {
+        bufferedWriter.write("M135 T0; select the extruder\r\n");
     }
 
     private void writePercentDone(BufferedWriter bufferedWriter, double linesToDo, double linesDone) throws IOException {
@@ -72,5 +101,28 @@ public class GcodeWing {
     private void setNextLayer(BufferedWriter bufferedWriter) throws IOException {
         currentZ += layerHeight;
         bufferedWriter.write(String.format("G1 X%.3f Y%.3f Z%.3f; next layer\r\n", currentX, currentY, currentZ));
+    }
+
+    private void writeLayer(BufferedWriter bufferedWriter, List<double[]> aerofoilData) throws IOException {
+        currentX = aerofoilData.get(0)[0] * targetChord;
+        currentY = aerofoilData.get(0)[1] * targetChord;
+        bufferedWriter.write(String.format("G1 X%.3f Y%.3f Z%.3f F%d; move\r\n", currentX, currentY, currentZ, travelSpeed));
+        bufferedWriter.write(String.format("G1 X%.3f Y%.3f Z%.3f F%d A%.5f; prime\r\n", currentX, currentY, currentZ, primeSpeed, currentA));
+        for (double[] dataElement : aerofoilData) {
+            final double nextX = dataElement[0] * targetChord;
+            final double nextY = dataElement[1] * targetChord;
+            final double filamentTravel = calculateFilamentUsed(currentX, currentY, nextX, nextY);
+            currentA += filamentTravel;
+            currentX = nextX;
+            currentY = nextY;
+            bufferedWriter.write(String.format("G1 X%.3f Y%.3f Z%.3f F%d A%.5f; data point\r\n", currentX, currentY, currentZ, extrudeSpeed, currentA));
+        }
+        bufferedWriter.write(String.format("G1 X%.3f Y%.3f Z%.3f F%d A%.5f; deprime\r\n", currentX, currentY, currentZ, primeSpeed, currentA - 1));
+    }
+
+    private double calculateFilamentUsed(double startX, double startY, double endX, double endY) {
+        double lengthA = (startY - endY);
+        double lengthB = (startX - endX);
+        return Math.sqrt(lengthA * lengthA + lengthB * lengthB) / 100;
     }
 }
